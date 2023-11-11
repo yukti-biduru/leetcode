c80e46e7714a0fb6254a346be3464b9c531985c1
Yukti Biduru <ybidur2@uic.edu>
2023-11-11 11:03:09 -0600

    Find Difference of Two Arrays

---
+++ Leetcode/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Leetcode.Solutions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = {1, 2, 3};
+        int[] arr2 = { 2, 4, 6 };
+        FindDifferenceInArrays solution = new FindDifferenceInArrays();
+        IList<IList<int>> output = solution.GetElementsOnlyInFirstList(arr, arr2);
+    }
+}
+
---
+++ Leetcode/Solutions/FindDifferenceInArrays.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Solutions
+{
+    public class FindDifferenceInArrays
+    {
+        public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+        {
+            List<int> result = nums1.ToList().Except(nums2.ToList()).ToList();
+            List<int> result1 = nums2.ToList().Except(nums1.ToList()).ToList();
+            IList<IList<int>> return_result = new List<IList<int>>
+            {
+                result,
+                result1
+            };
+            return return_result;
+        }
+
+        public IList<IList<int>> GetElementsOnlyInFirstList(int[] first, int[] second)
+        {
+            List<int> first1 = new List<int>();
+            List<int> second1 = new List<int>();
+            foreach (int i in first)
+            {
+                if (!second.Contains(i))
+                {
+                    first1.Add(i);
+                }
+            }
+            foreach (int i in second)
+            {
+                if (!first.Contains(i))
+                {
+                    second1.Add(i);
+                }
+            }
+            IList<IList<int>> return_result = new List<IList<int>>
+            {
+                first1.Distinct().ToList(),
+                second1.Distinct().ToList()
+            };
+            return return_result;
+        }
+    }
+
+}
